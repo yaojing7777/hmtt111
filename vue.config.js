71234680d3db53c61f78ddd038d3517e30f703ce
yaojing7777 <871266925@qq.com>
2022-06-26 09:45:08 +0800

    项目初始化

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,22 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // chainWebpack: config => {
+  //   // html-webpack-plugin
+  //   config
+  //     .plugin('html')
+  //     .tap(args => {
+  //       return [{
+  //         title: '相与枕藉乎舟中'
+  //       }]
+  //     })
+  // }
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].title = '相与枕藉乎舟中'
+        return args
+      })
+  }
 })
